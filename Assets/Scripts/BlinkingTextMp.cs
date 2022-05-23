d6c168a5e8183db6d7cad0906246dc163e22764a
adekalon <39853566+adekalon@users.noreply.github.com>
2022-05-24 01:47:26 +0400

    Initial commit

---
+++ Assets/Scripts/BlinkingTextMp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BlinkingTextMp : MonoBehaviour
+{
+    private float time = 1f;
+    private float timer;
+    private TMP_Text textFlicker;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textFlicker = GetComponent<TMP_Text>();
+        timer = time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            textFlicker.enabled = !textFlicker.enabled;
+            timer = time;
+        }
+    }
+}
